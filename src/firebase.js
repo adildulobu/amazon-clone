bdb34287db5625517cf5cbc8a08a8c0c44dfc631
adildulobu <adildulobu@gmail.com>
2022-02-22 11:47:56 +0200

    Firebase conf updated and cleaning app file

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA7PhEUJqM-2uG-XJYYlA7aUB821gScHQA",
   authDomain: "clone-97a81.firebaseapp.com",
@@ -13,5 +9,5 @@ const firebaseConfig = {
   appId: "1:122712607198:web:e012285a2f62dcf79f754f"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
