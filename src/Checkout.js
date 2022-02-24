9f06a20f575dcb1a8c39ecff0ccda21a069f1aa8
adildulobu <adildulobu@gmail.com>
2022-02-24 22:52:11 +0200

    Subtotal component created

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,8 +6,7 @@ function Checkout() {
   return (
     <div className='checkout'>
         <div className='checkout__left'>
-            <img classname='checkout__ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' alt=''/>
-            
+            <img className='checkout__ad' src='https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg' alt=''/>
             <div>
                 <h2 className='checkout__title'>Your shopping Basket</h2>
         
