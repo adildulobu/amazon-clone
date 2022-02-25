41a63ec356e406cc43380033216356d80fd65575
adildulobu <adildulobu@gmail.com>
2022-02-25 12:18:36 +0200

    login created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home'
 import Checkout from './Checkout';
+import Login from './Login';
 import {
   BrowserRouter as Router,
   Route,
@@ -9,15 +10,15 @@ import {
 } from "react-router-dom";
 
 
+
 function App() {
   return (
     <Router>
         <div className="App">
-           <Header/>
            <Routes>
-             <Route path='/checkout' element={<Checkout />}/>
-              <Route exact path='/' element={<Home />}
-              />
+             <Route path='/checkout' element={<> <Header/> <Checkout/></> }/>
+              <Route exact path='/' element={<> <Header/> <Home /> </>}/>
+              <Route path='/login' element={<Login />} />
             </Routes>
         </div>
     </Router>
--- src/Header.js
+++ src/Header.js
@@ -31,14 +31,16 @@ function Header() {
         </div>
 
         <div className='header__nav'>
-            <div className='header__option'>
-                <span className='header__optionLineOne'>
-                    Hello Guest
-                </span>
-                <span className='header__optionLineTwo'>
-                    SignIn
-                </span>
-            </div>
+            <Link to='/login'>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>
+                        Hello Guest
+                    </span>
+                    <span className='header__optionLineTwo'>
+                        SignIn
+                    </span>
+                </div>
+            </Link>
             <div className='header__option'>
             <span className='header__optionLineOne'>
                     Returns
